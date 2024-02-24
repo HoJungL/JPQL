607524f6f97115503f3a07397c4ae8c9ce6ed540
HoJungL <ghaud5684@naver.com>
2024-02-24 20:34:22 +0900

    JPQL 시작 --SQL과 문법은 거의 유사함.
    -- DTO를 만들어서 한번에 typequery 형태로 쓸수 있음. 단, 반드시 public DTO()를 형성해주어야함.
    
    -- 페이징은 select문을 String jpql = "select ~"라고 등록한뒤, em.createQuery에 대입하면 되는것!
    
    -- 서브쿼리 지원 함수
    (not) exists : 서브쿼리에 결과가 존재하면(하지않으면) 참
    All : 모두 만족하면 참
    Any, Some : 같은 의미이며, 조건을 하나라도 만족하면 참
    (not) in : 서브쿼리의 결과 중 하나라도 같은 것이 있으면(없으면) 참
    *JPA 서브 쿼리의 한계*
    jpa는 where, having절에만 서브쿼리 사용 가능
    select 절도 가능
    단, from 절의 서브쿼리는 현재 JPQL에서 불가능. 따라서 조인으로 풀 수 있으면 풀어서 해결해야한다.
    
    --Enumerated는 기본값이 숫자니까, 만약 JPQL 쿼리 조인에 넣으려는게 문자면 String으로 바꿔줘야함!

---
+++ src/main/java/jsql/JpqlMain_case.java
@@ -0,0 +1,59 @@
+package jsql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpqlMain_case {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            // 타입표현(admin)
+            member.setType(MemberType.ADMIN);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+
+            em.flush();
+            em.clear();
+
+            String query =
+                    "select" +
+                            " case when m.age <=10 then '학생요금'" +
+                            "      when m.age >=60 then '경로요금'" +
+                            "      else '일반요금'" +
+                            "end " +
+                    "from Member m";
+            List<String> result = em.createQuery(query, String.class)
+                    .getResultList();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
+
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
