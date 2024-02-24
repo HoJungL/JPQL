7b5284c7589f99ad6995f12dc946abd57cee9e84
HoJungL <ghaud5684@naver.com>
2024-02-24 15:34:15 +0900

    JPQL 시작

---
+++ src/main/java/jsql/Member.java
@@ -0,0 +1,41 @@
+package jsql;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int age;
+
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
